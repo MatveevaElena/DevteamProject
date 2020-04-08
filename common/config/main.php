d3f72e2a11e6764d9cdedef25d67a1f497beb30e
vasilenko-gs <vasilenko.gs@samgtu.ru>
2020-04-08 13:58:37 +0400

    config update

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,28 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+		'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '' => 'site/default',
+                '<controller:(\w+)>/<action:(\w+)>/<id:\d+>' => '<controller>/<action>',
+                '<controller:(\w+)>/<action:(\w+)>' => '<controller>/<action>',
+                '<module:(\w+)>/<controller:(\w+)>/<action:(\w+)>' => '<module>/<controller>/<action>',
+                '<module:(\w+)>/<controller:(\w+)>/<action:(\w+)>/<id:\d+>' => '<module>/<controller>/<action>',
+                '<a:(login|logout|request-password-reset)>' => 'site<a>',
+            ],
+        ], 'i18n' => [
+            'translations' => [
+                'ML*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'fileMap' => [
+                        'ML/Base' => 'ML.php',
+                        'ML' => 'ML.php',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
