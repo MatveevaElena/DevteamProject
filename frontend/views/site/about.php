40062b1b1be49079ed05a216ced6270667a01c96
IvanTabakerka <IvanTabakerka@yandex.ru>
2020-05-28 13:10:16 +0400

    Mobile Adaptation: About

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -14,9 +14,65 @@ $this->title = 'О сайте | Проектная платформа СамГТ
   <p>Мы и дальше будем поддерживать и развивать проектную платформу в лучшую сторону и надеемся, что этот инструмент поможет людям воплотить все свои идеи в жизнь.</p>
   <p>Удачи в ваших начинаниях!</p>
   <br><br>
-  <h2>Разработчики</h2>
+  <h2>Команда разработки</h2>
   <div class="line"></div>
   <div class="about_team">
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
+    <div class="about_team_elem">
+      <div class="about_team_elem_img">
+        <img src="/assets/img/about/1.png" alt="Изображение участника">
+      </div>
+      <h4>Федук</h4>
+      <p>Коллекционер курников</p>
+    </div>
     <div class="about_team_elem">
       <div class="about_team_elem_img">
         <img src="/assets/img/about/1.png" alt="Изображение участника">
